83d8d1122d1055c3e026830b444616c3ea1c447d
dfsharp <celo.89@gmail.com>
2016-05-16 16:48:51 -0500

    adds dev and build script

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ nconf.argv()
 .env()
 .file({file: __dirname + '/lib/config.json'});
 
-require('babel-register');
-require('./lib');
+if (process.env.NODE_ENV){
+    require('babel-register');
+    require('./lib');
+}
+else
+	require('./dist');
