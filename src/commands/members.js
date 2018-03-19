83129136b557f87be1f97e8efaa35fcb6dfe6984
Marcelo Adrián <DFsharp@users.noreply.github.com>
2018-03-19 12:32:31 -0600

    Membership (#5)
    * ignore .vs
    
    * add admin channel id
    
    * modifies !join to send a message to admin channel instead of giving role.
    
    * fixes guild check

--- src/commands/members.js
+++ src/commands/members.js
@@ -1,16 +1,18 @@
 import nconf from 'nconf';
 
+const membershipId = nconf.get("MEMBERSHIP_ROLE_ID");
+const adminChannelId = nconf.get("ADMIN_CHANNEL_ID");
+
 export default {
   join: (msg, suffix) => {
-    let roleId = nconf.get("MEMBERSHIP_ROLE_ID");
-    if (roleId) {
-      if (!msg.member.roles.find("id", roleId)) {
-        msg.member.addRole(`${roleId}`)
-          .then(
-            (fulfillment) => {
-              msg.reply("Welcome to OUTER::HAVEN");
-            },
-            (rejection) => { });
+    if (!membershipId || !adminChannelId || !msg.guild) return;
+
+    if (!msg.member.roles.find("id", membershipId)) {
+      const adminChannel = msg.guild.channels.get(adminChannelId);
+
+      if (adminChannel) {
+        msg.reply("request sent to Big Boss.");
+        adminChannel.send(`${msg.member} would like to join ${msg.guild.name}`);
       }
     }
   }
